417c2a4b426710557f4b4590d6b141d4c7900ffa
Apricot-S <mahjong_apricot@outlook.jp>
2024-05-31 03:31:09 +0900

    refactor: rename

--- src/rank_predictor/validator.py
+++ src/rank_predictor/validator.py
@@ -2,18 +2,18 @@ from typing import Final, assert_never
 
 from rank_predictor.types import NumPlayer
 
-MAX_INPUT_SCORE_4: Final[int] = 1_000
-MAX_INPUT_SCORE_3: Final[int] = 1_050
+TOTAL_SCORE_4: Final[int] = 1_000
+TOTAL_SCORE_3: Final[int] = 1_050
 
 
 def validate_input_score_4(score: int) -> None:
-    if score < 0 or score > MAX_INPUT_SCORE_4:
+    if score < 0 or score > TOTAL_SCORE_4:
         msg = f"Invalid score is input. : {score}"
         raise ValueError(msg)
 
 
 def validate_input_score_3(score: int) -> None:
-    if score < 0 or score > MAX_INPUT_SCORE_3:
+    if score < 0 or score > TOTAL_SCORE_3:
         msg = f"Invalid score is input. : {score}"
         raise ValueError(msg)
 
