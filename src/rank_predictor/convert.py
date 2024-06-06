1af2fa0d4cbb669c22e2b3025fd235bbcd6d08ad
Apricot-S <mahjong_apricot@outlook.jp>
2024-06-06 22:28:51 +0900

    feat: develop

--- src/rank_predictor/convert.py
+++ src/rank_predictor/convert.py
@@ -1,3 +1,4 @@
+from dataclasses import dataclass
 from enum import IntFlag
 from logging import getLogger
 from pathlib import Path
@@ -15,6 +16,101 @@ MJLOG_ROOT_TAG: Final = "mjloggm"
 class TenhouGameType(IntFlag):
     IS_HANCHAN = 0x008
     IS_THREE_PLAYER = 0x010
+
+
+@dataclass
+class RoundState:
+    round_: int
+    num_counter_stick: int
+    num_riichi_deposit: int
+
+
+def parse_seed(seed: str) -> RoundState | None:
+    seed_list = seed.split(",")
+    if len(seed_list) != 6:  # noqa: PLR2004
+        logger.warning("The number of `seed` is invalid.: %s", seed_list)
+        return None
+
+    try:
+        seed_numbers = [int(x) for x in seed_list]
+    except ValueError:
+        logger.warning(
+            "`seed` contains non-numeric characters.: %s",
+            seed_list,
+        )
+        return None
+
+    round_ = seed_numbers[0]
+    if round_ < 0 or round_ > 11:  # noqa: PLR2004
+        logger.warning("The number of round is invalid.: %s", round_)
+        return None
+
+    num_counter_stick = seed_numbers[1]
+    if num_counter_stick < 0:
+        logger.warning(
+            "The number of counter sticks is invalid.: %s",
+            num_counter_stick,
+        )
+        return None
+
+    num_riichi_deposit = seed_numbers[2]
+    if num_riichi_deposit < 0:
+        logger.warning(
+            "The number of riichi deposits is invalid.: %s",
+            num_riichi_deposit,
+        )
+        return None
+
+    return RoundState(
+        round_=round_,
+        num_counter_stick=num_counter_stick,
+        num_riichi_deposit=num_riichi_deposit,
+    )
+
+
+def parse_score(score: str, num_player: NumPlayer) -> list[int] | None:
+    score_list = score.split(",")
+    if len(score_list) != num_player:
+        logger.warning("The number of `ten` is invalid.: %s", score_list)
+        return None
+
+    try:
+        score_numbers = [int(x) for x in score_list]
+    except ValueError:
+        logger.warning(
+            "`ten` contains non-numeric characters.: %s",
+            score_list,
+        )
+        return None
+
+    if any(p < 0 for p in score_numbers):
+        logger.warning(
+            "A negative score is included at the start of the round.: %s",
+            score_numbers,
+        )
+        return None
+
+    return score_numbers
+
+
+def parse_result(result: str, num_player: NumPlayer) -> list[int] | None:
+    result_list = result.split(",")
+    if len(result_list) != (num_player * 2):
+        logger.warning("The number of `owari` is invalid.: %s", result_list)
+        return None
+
+    score_list = result_list[::2]
+
+    try:
+        score_numbers = [int(x) for x in score_list]
+    except ValueError:
+        logger.warning(
+            "The scores of `owari` contains non-numeric characters.: %s",
+            result_list,
+        )
+        return None
+
+    return score_numbers
 
 
 def convert(
@@ -49,25 +145,30 @@ def convert(
 
     logger.info("Conversion target: %s-Player, %s", num_player, length_name)
 
+    with training_data.open("w") as f:
+        f.write(
+            "round,num_counter_stick,num_riichi_deposit,"
+            "current_score_0,current_score_1,current_score_2,current_score_3,"
+            "final_score_0,final_score_1,final_score_2,final_score_3\n",
+        )
+
     for file in game_record_dir.glob("*.xml"):
+        logger.info("Parsing... : %s", file.name)
         tree = ElementTree.parse(file)
         root = tree.getroot()
 
         if root.tag != MJLOG_ROOT_TAG:
-            logger.warning("This file is not in mjlog format.: %s", file.name)
+            logger.warning("This file is not in mjlog format.")
             continue
 
         go = root.find("GO")
         if go is None:
-            logger.warning("`GO` tag is not included.: %s", file.name)
+            logger.warning("`GO` tag is not included.")
             continue
 
-        game_type = go.attrib.get("type")
+        game_type = go.get("type")
         if not isinstance(game_type, str):
-            logger.warning(
-                "`GO` tag is missing a `type` attribute.: %s",
-                file.name,
-            )
+            logger.warning("`GO` tag is missing a `type` attribute.")
             continue
 
         try:
@@ -93,3 +194,58 @@ def convert(
         if log_game_length != game_length:
             logger.info("This mjlog is not a target.: %s", log_game_length)
             continue
+
+        inits = root.findall("INIT")
+        if not inits:
+            logger.warning("`INIT` tag is not included.")
+            continue
+
+        states: list[RoundState] = []
+        scores: list[list[int]] = []
+        for init in inits:
+            seed = init.get("seed")
+            if not isinstance(seed, str):
+                logger.warning("`INIT` tag is missing a `seed` attribute.")
+                continue
+
+            state = parse_seed(seed)
+            if state is None:
+                continue
+
+            ten = init.get("ten")
+            if not isinstance(ten, str):
+                logger.warning("`INIT` tag is missing a `ten` attribute.")
+                continue
+
+            score = parse_score(ten, log_num_player)
+            if score is None:
+                continue
+
+            states.append(state)
+            scores.append(score)
+
+        owari = None
+        agaris = root.findall("AGARI")
+        owari = agaris[-1].get("owari")
+        if owari is None:
+            ryuukyokus = root.findall("RYUUKYOKU")
+            owari = ryuukyokus[-1].get("owari")
+
+        if owari is None:
+            logger.warning("There is no score at the end of the game.")
+            continue
+
+        result = parse_result(owari, log_num_player)
+        if result is None:
+            continue
+
+        with training_data.open("a") as f:
+            for st, sc in zip(states, scores, strict=True):
+                f.write(f"{st.round_},")
+                f.write(f"{st.num_counter_stick},")
+                f.write(f"{st.num_riichi_deposit},")
+                for s in sc:
+                    f.write(f"{s},")
+                for r in result:
+                    f.write(f"{r},")
+                f.write("\n")
