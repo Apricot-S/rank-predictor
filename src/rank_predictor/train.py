fea989877cabe2a4ee19babe06703300a8b7b123
Apricot-S <mahjong_apricot@outlook.jp>
2024-06-11 12:33:27 +0900

    refactor: Convert validation processing into a function

--- src/rank_predictor/train.py
+++ src/rank_predictor/train.py
@@ -10,9 +10,9 @@ from rank_predictor.types import (
     DataName,
     GameLength,
     NumPlayer,
-    Round,
     get_game_length_name,
 )
+from rank_predictor.validate import validate_annotated_data
 
 logger = getLogger(__name__)
 
@@ -40,6 +40,8 @@ def train(
         get_game_length_name(game_length),
     )
 
+    validate_annotated_data(num_player, game_length, training_data)
+
     feature_columns = [
         DataName.ROUND,
         DataName.NUM_COUNTER_STICK,
@@ -47,57 +49,6 @@ def train(
         *[f"{DataName.SCORE}_{i}" for i in range(num_player)],
     ]
     label_column = DataName.RANK_CLASS
-
-    required_columns = [*feature_columns, label_column]
-    columns = training_data.columns
-    missing_columns = [r for r in required_columns if r not in columns]
-    if missing_columns:
-        msg = f"Training data is missing columns: {missing_columns}"
-        raise ValueError(msg)
-
-    if (num_player == NumPlayer.THREE) and (f"{DataName.SCORE}_3" in columns):
-        msg = (
-            "The training target is 3-player,"
-            f" but the data contains `{DataName.SCORE}_3`."
-        )
-        raise ValueError(msg)
-
-    invalid_round = (
-        Round.WEST_1 if game_length == GameLength.TONPU else (Round.WEST_4 + 1)
-    )
-    num_invalid_round = (
-        training_data.lazy()
-        .filter(pl.col(DataName.ROUND) >= invalid_round)
-        .select(pl.len())
-        .collect()
-        .item()
-    )
-    if num_invalid_round > 0:
-        round_name = "West" if game_length == GameLength.TONPU else "North"
-        msg = (
-            f"The training target is {get_game_length_name(game_length)},"
-            f" but the data contains rounds after {round_name}-1."
-        )
-        raise ValueError(msg)
-
-    def validate_no_negative_values(column: str) -> None:
-        negative_count = (
-            training_data.lazy()
-            .filter(pl.col(column) < 0)
-            .select(pl.len())
-            .collect()
-            .item()
-        )
-        if negative_count > 0:
-            msg = f"`{column}` data contains negative values."
-            raise ValueError(msg)
-
-    validate_no_negative_values(DataName.ROUND)
-    validate_no_negative_values(DataName.NUM_COUNTER_STICK)
-    validate_no_negative_values(DataName.NUM_RIICHI_DEPOSIT)
-    for i in range(num_player):
-        validate_no_negative_values(f"{DataName.SCORE}_{i}")
-
     feature = training_data.select(feature_columns)
     label = training_data.select(label_column).to_series()
 
--- src/rank_predictor/validate.py
+++ src/rank_predictor/validate.py
@@ -1,9 +1,81 @@
 from typing import Final, assert_never
 
-from rank_predictor.types import NumPlayer
+import polars as pl
+
+from rank_predictor.types import (
+    DataName,
+    GameLength,
+    NumPlayer,
+    Round,
+    get_game_length_name,
+)
 
 TOTAL_SCORE_4: Final[int] = 1_000
 TOTAL_SCORE_3: Final[int] = 1_050
+
+
+def validate_annotated_data(
+    num_player: NumPlayer,
+    game_length: GameLength,
+    annotated_data: pl.DataFrame,
+) -> None:
+    feature_columns = [
+        DataName.ROUND,
+        DataName.NUM_COUNTER_STICK,
+        DataName.NUM_RIICHI_DEPOSIT,
+        *[f"{DataName.SCORE}_{i}" for i in range(num_player)],
+    ]
+    label_column = DataName.RANK_CLASS
+
+    required_columns = [*feature_columns, label_column]
+    columns = annotated_data.columns
+    missing_columns = [r for r in required_columns if r not in columns]
+    if missing_columns:
+        msg = f"Training data is missing columns: {missing_columns}"
+        raise ValueError(msg)
+
+    if (num_player == NumPlayer.THREE) and (f"{DataName.SCORE}_3" in columns):
+        msg = (
+            "The training target is 3-player,"
+            f" but the data contains `{DataName.SCORE}_3`."
+        )
+        raise ValueError(msg)
+
+    invalid_round = (
+        Round.WEST_1 if game_length == GameLength.TONPU else (Round.WEST_4 + 1)
+    )
+    num_invalid_round = (
+        annotated_data.lazy()
+        .filter(pl.col(DataName.ROUND) >= invalid_round)
+        .select(pl.len())
+        .collect()
+        .item()
+    )
+    if num_invalid_round > 0:
+        round_name = "West" if game_length == GameLength.TONPU else "North"
+        msg = (
+            f"The training target is {get_game_length_name(game_length)},"
+            f" but the data contains rounds after {round_name}-1."
+        )
+        raise ValueError(msg)
+
+    def validate_no_negative_values(column: str) -> None:
+        negative_count = (
+            annotated_data.lazy()
+            .filter(pl.col(column) < 0)
+            .select(pl.len())
+            .collect()
+            .item()
+        )
+        if negative_count > 0:
+            msg = f"`{column}` data contains negative values."
+            raise ValueError(msg)
+
+    validate_no_negative_values(DataName.ROUND)
+    validate_no_negative_values(DataName.NUM_COUNTER_STICK)
+    validate_no_negative_values(DataName.NUM_RIICHI_DEPOSIT)
+    for i in range(num_player):
+        validate_no_negative_values(f"{DataName.SCORE}_{i}")
 
 
 def validate_input_score_4(score: int) -> None:
