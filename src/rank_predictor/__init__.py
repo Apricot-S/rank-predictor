0f632719e5eba808c5eef445140d3fecfca67ca6
Apricot-S <mahjong_apricot@outlook.jp>
2024-06-13 09:39:33 +0900

    feat: export a type

--- src/rank_predictor/__init__.py
+++ src/rank_predictor/__init__.py
@@ -1,10 +1,11 @@
 from logging import NullHandler, getLogger
 
-from rank_predictor.types import GameLength, NumPlayer
+from rank_predictor.types import GameLength, NumPlayer, Round
 
 getLogger(__name__).addHandler(NullHandler())
 
 __all__ = [
     "GameLength",
     "NumPlayer",
+    "Round",
 ]
