1c6a4e5d749101e4d409a6fb199cbb5704e5d21f
Apricot-S <mahjong_apricot@outlook.jp>
2024-06-18 17:46:00 +0900

    docs: add docstring for split.py

--- src/rank_predictor/split.py
+++ src/rank_predictor/split.py
@@ -1,3 +1,5 @@
+"""Provides functionality to split data."""
+
 from pathlib import Path
 
 import polars as pl
@@ -17,6 +19,31 @@ def split(
     shuffle: bool = True,
     stratify: bool = False,
 ) -> None:
+    """Splits the input data into training and test datasets.
+
+    This function wraps scikit-learn's `train_test_split` and saves the
+    resulting datasets to the specified paths.
+
+    Args:
+        input_data_path: The path to the input data file.
+        train_data_path: The path where the training data will be saved.
+        test_data_path: The path where the test data will be saved.
+        test_size: The proportion or absolute number of the dataset to
+            include in the test split. Refer to the scikit-learn
+            documentation for `train_test_split` for more details.
+            Defaults to None.
+        train_size: The proportion or absolute number of the dataset to
+            include in the train split. Refer to the scikit-learn
+            documentation for `train_test_split` for more details.
+            Defaults to None.
+        random_state: The seed used by the random number generator.
+            Refer to the scikit-learn documentation for
+            `train_test_split` for more details. Defaults to None.
+        shuffle: Whether or not to shuffle the data before splitting. If
+            shuffle=False then stratify must be False. Defaults to True.
+        stratify: If True, data is split in a stratified fashion, using
+            `rank_class` as the class labels. Defaults to False.
+    """
     input_data = pl.read_csv(input_data_path)
 
     label = None
