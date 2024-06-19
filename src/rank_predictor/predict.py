1c7e54948d6911b8f073fd392049208edbf2465e
Apricot-S <mahjong_apricot@outlook.jp>
2024-06-19 15:16:18 +0900

    docs: add docstring for predict.py

--- src/rank_predictor/predict.py
+++ src/rank_predictor/predict.py
@@ -1,3 +1,5 @@
+"""Provides functionality to predict expected final rank."""
+
 from collections.abc import Sequence
 
 import numpy as np
@@ -14,6 +16,17 @@ def create_feature(
     num_riichi_deposit: int,
     score: Sequence[int],
 ) -> pl.DataFrame:
+    """Creates a features based on the current state of a round.
+
+    Args:
+        round_: The current round.
+        num_counter_stick: The number of counter sticks.
+        num_riichi_deposit: The number of riichi deposits.
+        score: The current scores of the players.
+
+    Returns:
+        A DataFrame containing the features.
+    """
     data: dict[str, list[int]] = {
         DataName.ROUND: [round_],
         DataName.NUM_COUNTER_STICK: [num_counter_stick],
@@ -25,6 +38,19 @@ def create_feature(
 
 
 def predict_proba(model: Model, feature: pl.DataFrame) -> np.ndarray:
+    """Predicts the probabilities for each rank class.
+
+    For more information on rank classes, refer to
+    `rank_predictor.rank.RANK_CLASS_4` and
+    `rank_predictor.rank.RANK_CLASS_3`.
+
+    Args:
+        model: The model to use for prediction.
+        feature: The features to use as input for prediction.
+
+    Returns:
+        An array of the probabilities for each rank class.
+    """
     return model.classifier.predict_proba(feature)[0]
 
 
@@ -32,6 +58,35 @@ def calculate_player_rank_proba(
     num_player: NumPlayer,
     proba: np.ndarray,
 ) -> np.ndarray:
+    """Calculates the probabilities for each player's rank.
+
+    Args:
+        num_player: The number of players.
+        proba: The probabilities for each rank class.
+
+    Returns:
+        An array of the probabilities for each player's rank.
+
+    Examples:
+        >>> proba = np.array(
+        ...     [
+        ...         0.2083811,
+        ...         0.04574999,
+        ...         0.13748033,
+        ...         0.22065941,
+        ...         0.22998391,
+        ...         0.15774526,
+        ...     ]
+        ... )
+        >>> player_rank_proba = calculate_player_rank_proba(
+        ...     NumPlayer.THREE,
+        ...     proba,
+        ... )
+        >>> print(player_rank_proba)
+        [[0.2541311  0.36746423 0.37840467]
+         [0.35813974 0.36612636 0.2757339 ]
+         [0.38772917 0.26640941 0.34586143]]
+    """
     player_rank_proba = np.zeros((num_player, num_player))
     for player in range(num_player):
         for rank in range(num_player):
@@ -41,5 +96,25 @@ def calculate_player_rank_proba(
 
 
 def calculate_expected_rank(player_rank_proba: np.ndarray) -> np.ndarray:
+    """Calculates the expected rank for each player.
+
+    Args:
+        player_rank_proba: The probabilities of each player's rank.
+
+    Returns:
+        An array of the expected ranks for each player.
+
+    Examples:
+        >>> player_rank_proba = np.array(
+        ...     [
+        ...         [0.25413110, 0.36746423, 0.37840467],
+        ...         [0.35813974, 0.36612636, 0.27573390],
+        ...         [0.38772917, 0.26640941, 0.34586143],
+        ...     ]
+        ... )
+        >>> expected_ranks = calculate_expected_rank(player_rank_proba)
+        >>> print(expected_ranks)
+        [2.12427357 1.91759416 1.95813226]
+    """
     ranks = np.arange(1, player_rank_proba.shape[1] + 1)
     return np.sum(player_rank_proba * ranks, axis=1)
